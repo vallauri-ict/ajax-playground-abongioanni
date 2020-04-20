8566f55d1cf8904c64e48bb1fe03ca0b8c39f28c
Alberto Bongioanni <a.bongioanni.0746@vallauri.edu>
2020-04-20 15:13:05 +0200

    prova di commit da vscode

--- alphaVantage/index.js
+++ alphaVantage/index.js
@@ -51,7 +51,7 @@ $(document).ready(function () {
             appendTo:_cmb
         });
     }
-    //$(_cmb).trigger("change");
+    $(_cmb).trigger("change");
 
     function createRow(data){
         let _tr = $("<tr>");
@@ -62,5 +62,25 @@ $(document).ready(function () {
             });
         }
         return _tr;
+    }
+
+    function inviaRichiesta(method, url, parameters = "") {
+        return $.ajax({
+            type: method,
+            url: url,
+            data: parameters,
+            contentType: "application/x-www-form-urlencoded;charset=utf-8",
+            dataType: "json",
+            timeout: 5000
+        });
+    }
+    
+    function error(jqXHR, text_status, string_error) {
+        if (jqXHR.status == 0)
+            alert("Connection Refused or Server timeout");
+        else if (jqXHR.status == 200)
+            alert("Formato dei dati non corretto : " + jqXHR.responseText);
+        else
+            alert("Server Error: " + jqXHR.status + " - " + jqXHR.responseText);
     }
 });

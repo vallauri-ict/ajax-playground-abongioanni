9ac3049b57b6107e9bf6a6fb7e40c7d5ea6a0b77
Alberto Bongioanni <a.bongioanni.0746@vallauri.edu>
2020-04-21 15:47:25 +0200

    Aggiunta di JSON Server e Gestione Grafico con HartJS

--- alphaVantage/index.js
+++ alphaVantage/index.js
@@ -1,27 +1,35 @@
 "use strict";
 
-const apiKey="UJGWAY47OK4QKSZ0";
-const v={
-    "IBM":"IBM",
-    "MSFT":"Microsoft Corporation",
-    "BABA":"Alibaba",
-    "XIACF":"Xiaomi Corporation",
-    "SNE":"Sony Corporation"
-};
+const apiKey = "UJGWAY47OK4QKSZ0";
 
 $(document).ready(function () {
-
-    let _table=$("#tblResults tbody");
-    let _cmb=$("#cmbSymbols").on("change",function () {
-        let request=inviaRichiesta("GET","https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol="+$(this).val()+"&apikey="+apiKey);
+    let _table = $("#tblResults tbody");
+    //EVENT COMBO CHANGE
+    let _cmb = $("#cmbSymbols").on("change", function () {
+        let request = inviaRichiesta("GET", "http://localhost:3000/GLOBAL_QUOTES?symbol=" + $(this).val() /*+ "&apikey=" + apiKey*/);
         request.done(function (data) {
-            $(_table).html("").append(createRow(data["Global Quote"]));
+            $(_table).html("").append(createRow(data[0]["Global Quote"]));
+            console.log(data);
         });
         request.fail(error);
     });
-    let _txtSearch=$("#txtSearch").on("keyup",function(){
-        if($(this).val().length>=2){
-            let request=inviaRichiesta("GET","https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords="+$(this).val()+"&apikey="+apiKey);
+    //CARICAMENTO COMBO
+    let companiesR = inviaRichiesta("GET", "http://localhost:3000/companies");
+    companiesR.done(function (data) {
+        for (let i = 0; i < data.length; i++) {
+            $("<option>", {
+                text: data[i]["desc"],
+                value: data[i]["id"],
+                appendTo: _cmb
+            });
+        }
+        $(_cmb).trigger("change");
+    });
+    companiesR.fail(error);
+    //RICERCA INCREMENTALE
+    let _txtSearch = $("#txtSearch").on("keyup", function () {
+        if ($(this).val().length >= 2) {
+            let request = inviaRichiesta("GET", "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + $(this).val() + "&apikey=" + apiKey);
             request.done(function (data) {
                 $(_table).html("");
                 try {
@@ -30,7 +38,7 @@ $(document).ready(function () {
                         r.done(function (data) {
                             $(_table).append(createRow(data["Global Quote"]));
                             if ("Note" in data) {
-                                i=5;
+                                i = 5;
                                 throw "Limite richieste al server raggiunto\n" + data["Note"];
                             }
                         });
@@ -43,17 +51,28 @@ $(document).ready(function () {
             request.fail(error);
         }
     });
-
-    for (let key in v){
-        $("<option>",{
-            text:v[key],
-            value:key,
-            appendTo:_cmb
+    //GRAFICO
+    let r = inviaRichiesta("GET", "http://localhost:3000/chart");
+    r.done(function (data) {
+        let chart=data;
+        let chartReq = inviaRichiesta("GET", "http://localhost:3000/3year");
+        chartReq.done(function (data) {
+            for (let key in data) {
+                let d=chart["data"];
+                d["labels"].push(key);
+                let dataset=d["datasets"][0];
+                dataset["data"].push(data[key].replace("%",""));
+                let color="rgba("+Random(0,255)+", "+Random(0,255)+", "+Random(0,255)+", 1)";
+                dataset["backgroundColor"].push(color);
+                dataset["borderColor"].push(color);
+            }
+            console.log(chart);
+            var myBarChart = new Chart($("#myChart"), chart);
         });
-    }
-    $(_cmb).trigger("change");
-
-    function createRow(data){
+    });
+    r.fail(error);
+    //FUNCTIONS
+    function createRow(data) {
         let _tr = $("<tr>");
         for (let key in data) {
             $("<td>", {
@@ -74,7 +93,7 @@ $(document).ready(function () {
             timeout: 5000
         });
     }
-    
+
     function error(jqXHR, text_status, string_error) {
         if (jqXHR.status == 0)
             alert("Connection Refused or Server timeout");
@@ -83,4 +102,9 @@ $(document).ready(function () {
         else
             alert("Server Error: " + jqXHR.status + " - " + jqXHR.responseText);
     }
+
+    function Random(min, max) {
+        return Math.floor((max - min + 1) * Math.random()) + min;
+    }
+
 });

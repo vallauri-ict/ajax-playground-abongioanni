50358fec32a219631a4f58c0b6d4eb7b69f78749
Alberto Bongioanni <a.bongioanni.0746@vallauri.edu>
2020-05-02 22:38:44 +0200

    Update

--- alphaVantage/index.js
+++ alphaVantage/index.js
@@ -2,12 +2,30 @@
 
 const apiKey = "UJGWAY47OK4QKSZ0";
 
+
+let credentials = {
+    "web": {
+        "client_id": "461420881554-vohtumtl57fgfk63ft022vb2mqa4ufkl.apps.googleusercontent.com",
+        "project_id": "my-project-46576-275713",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "client_secret": "vAGVkxVhQNHTdJNgMtrVthOj",
+        "redirect_uris": [
+            "http://127.0.0.1:8080/index.html"
+        ],
+        "javascript_origins": [
+            "http://127.0.0.1:8080"
+        ]
+    }
+}
+
+const clientSecret = credentials["web"]["client_secret"];
+let redirectUri = credentials["web"]["redirect_uris"][0];
+let scope = "https://www.googleapis.com/auth/drive";
+let clientId = credentials["web"]["client_id"];
 const urlParams = new URLSearchParams(window.location.search);
 const code = urlParams.get('code');
-const client_secret = "vAGVkxVhQNHTdJNgMtrVthOj";
-let redirect_uri = "http://127.0.0.1:8080/index.html";
-let scope = "https://www.googleapis.com/auth/drive";
-let clientId = "461420881554-vohtumtl57fgfk63ft022vb2mqa4ufkl.apps.googleusercontent.com";
 
 $(document).ready(function () {
     $(".loader .logo").fadeOut(0).fadeIn(1000);
@@ -22,7 +40,7 @@ $(document).ready(function () {
 
     let c;//chart is empty
     if (localStorage.getItem("accessToken") === null) {
-        $(".googleIcon").addClass("grey");
+        $(".googleIcon").addClass("grey").prop("title","Non sei registrato");
     }
 
     //CARICAMENTO COMBO SETTORE
@@ -67,7 +85,7 @@ $(document).ready(function () {
             ]
         });//AGGIORNAMENTO DEL MENU' RESPONSIVE
         if (!$(_a).prop("class").includes("googleIcon")) {
-            _newa.href = _a.href;
+            _newa.prop("href",_a.prop("href"));
         }
     }
 
@@ -83,21 +101,54 @@ $(document).ready(function () {
         $(_responsiveLinks).animate({ opacity: 0 }, 125, () => { $(_responsiveLinks).parent().removeClass("open") });
     });
 
-    $(".googleIcon").click(function () {
+    $(".googleIcon").on("click", function () {
+        if ($("#files").val() == "") {
+            $("#uploadResult")
+                .removeClass("alert-danger").removeClass("alert-success")
+                .addClass("alert-warning")
+                .html("<b>Attenzione</b>: devi selezionare un file!")
+                .fadeOut(0)
+                .fadeIn();
+            return;
+        }
         if (localStorage.getItem("accessToken") === null) {
-            signIn(clientId, redirect_uri, scope);
+            signIn(clientId, clientSecret, redirectUri, scope);
         }
         else {
             var file = $("#files")[0].files[0];
-            var upload = new Upload(file);
-            upload.doUpload();
-            alert("Elemento Caricato!");
+            let upload_ = new Upload(file).doUpload();
+            upload_.done(function (data) {
+                $("#uploadResult")
+                    .removeClass("alert-danger").removeClass("alert-warning")
+                    .addClass("alert-success")
+                    .html("<b>Successo</b>: il caricamento è andato a buon fine!")
+                    .fadeOut(0).fadeIn();
+            }).fail(function () {
+                $("#uploadResult")
+                    .removeClass("alert-success").removeClass("alert-warning")
+                    .addClass("alert-danger")
+                    .html("<b>Errore</b>: C'è stato un errore durante il caricamento")
+                    .fadeOut(0)
+                    .fadeIn();
+            });
         }
     });
 
     $("#downloadChart").on("click", function () {
         let url = c.toBase64Image()
         $(this).prop("href", url);
+    });
+
+    $("#files").on("change", function () {
+        var fullPath = $(this).val();
+        if (fullPath) {
+            var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+            var filename = fullPath.substring(startIndex);
+            if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+                filename = filename.substring(1);
+            }
+            $("label[for=files]").text(filename);
+        }
     });
 
     //EVENT COMBO COMPANIES CHANGE EVENT
@@ -190,15 +241,15 @@ $(document).ready(function () {
     }
 
     /***************************** FUNCTIONS *****************************/
-    function signIn(clientId, redirect_uri, scope) {
-        let url = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=" + redirect_uri
+    function signIn(clientId, clientSecret, redirectUri, scope) {
+        let url = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=" + redirectUri
             + "&prompt=consent&response_type=code&client_id=" + clientId + "&scope=" + scope
             + "&access_type=offline";
         let r_ = inviaRichiesta("POST", "https://www.googleapis.com/oauth2/v4/token",
             {
-                code: code
-                , redirect_uri: redirect_uri,
-                client_secret: client_secret,
+                code: code,
+                redirect_uri: redirectUri,
+                client_secret: clientSecret,
                 client_id: clientId,
                 scope: scope,
                 grant_type: "authorization_code"
@@ -239,7 +290,7 @@ $(document).ready(function () {
         formData.append("file", this.file, this.getName());
         formData.append("upload_file", true);
 
-        $.ajax({
+        return $.ajax({
             type: "POST",
             beforeSend: function (request) {
                 request.setRequestHeader("Authorization", "Bearer" + " " + localStorage.getItem("accessToken"));
@@ -252,12 +303,6 @@ $(document).ready(function () {
             xhr: function () {
                 var myXhr = $.ajaxSettings.xhr();
                 return myXhr;
-            },
-            success: function (data) {
-                console.log(data);
-            },
-            error: function (error) {
-                console.log(error);
             },
             async: true,
             data: formData,
@@ -303,6 +348,6 @@ $(document).ready(function () {
     function Random(min, max) {  //INCLUSIVO
         return Math.floor((max - min + 1) * Math.random()) + min;
     }
-    setTimeout(function(){$(".loader").slideUp();},1500);
-    
+    setTimeout(function () { $(".loader").slideUp(); $(".loader .lds-ring").fadeOut(); }, 1500);
+
 });
